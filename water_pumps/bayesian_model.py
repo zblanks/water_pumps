46128126e607907d93031ebd472bacfe3f954152
Zachary Blanks <zachblanks17@gmail.com>
2021-11-20 17:36:57 -0500

    Initial working uninformed model with MCMC

--- water_pumps/bayesian_model.py
+++ water_pumps/bayesian_model.py
@@ -1,14 +1,19 @@
-from jax import random
+from jax import random, vmap
 import jax.numpy as jnp
+import numpy as np
 import numpy.typing as npt
 import numpyro
 import numpyro.distributions as dist
 from numpyro.infer import MCMC, NUTS
 
 
+def compute_logit(α, θ, x):
+    return α + jnp.dot(θ, x)
+
+
 def uninformed_model(X: npt.NDArray, regions: npt.NDArray, y: npt.NDArray):
-    n, p = X.size()
-    n_regions = len(regions)
+    n, p = X.shape
+    n_regions = len(np.unique(regions))
 
     # Uninformed hyper-priors for the one-level hierarchical model
     μ_a = numpyro.sample('μ_a', dist.Normal(0., 100.))
@@ -16,15 +21,15 @@ def uninformed_model(X: npt.NDArray, regions: npt.NDArray, y: npt.NDArray):
 
     μ_t = numpyro.sample('μ_t', dist.Normal(0., 100.))
     σ_t = numpyro.sample('σ_t', dist.HalfCauchy(5.))
-    μ = jnp.repeat([μ_t], repeats=p)
-    Σ = jnp.diag(jnp.repeat([σ_t], repeats=p))
+    μ = jnp.repeat(μ_t, repeats=p)
+    Σ = jnp.diag(jnp.repeat(σ_t, repeats=p))
 
     # Create an intercept for each administrative region
     with numpyro.plate('regions', n_regions):
         α = numpyro.sample('α', dist.Normal(μ_a, σ_a))
         θ = numpyro.sample('θ', dist.MultivariateNormal(μ, Σ))
     
-    β = α[regions] + jnp.dot(X, θ[regions])
+    β = vmap(compute_logit, in_axes=0)(α[regions], θ[regions], X)
     with numpyro.plate('samples', n):
         numpyro.sample('y', dist.Bernoulli(logits=β), obs=y)
 
