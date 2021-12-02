94386385085d334cc37995962b9dcfc248caa9bd
Zachary Blanks <zachblanks17@gmail.com>
2021-12-01 21:01:03 -0500

    Created repair location map

--- water_pumps/run_experiment.py
+++ water_pumps/run_experiment.py
@@ -6,10 +6,12 @@ import dill
 from itertools import product
 from jax import vmap
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
 import numpy as np
 from optimization_model import WaterPointRepair
 import os
 import pandas as pd
+import shapefile as shp
 from sklearn.metrics import classification_report
 
 
@@ -74,6 +76,56 @@ def fix_waterpoints(X, regions, model, df, Γs, Bs, region='Dar es Salaam'):
     return zs
 
 
+def get_repair_idx(df, z, region):
+    idx = [df.index.get_loc(val) for val in df[df.region == region].index] 
+    idx_map = dict(zip(range(len(idx)), idx))
+    repair_idx = np.where(z == 1)[0]
+    repair_idx = [idx_map[val] for val in repair_idx]
+    return repair_idx
+
+
+def get_region_bounds(shp_file):
+    shape = shp_file.shape(1) # DES is the 2nd administrative region
+    i0 = shape.parts[2] # DES has two small uninhabitated islands
+    i1 = len(shape.points)
+    seg = shape.points[i0:(i1 + 1)]
+    x = [seg[i][0] for i in range(len(seg))]
+    y = [seg[i][1] for i in range(len(seg))]
+    return x, y
+
+
+def plot_repairs(df, y_bound, z_star, key, coords, c, fig=None, ax=None, region='Dar es Salaam'):
+    z = z_star[key]
+    idx = [df.index.get_loc(val) for val in df[df.region == region].index] 
+    repair_idx = get_repair_idx(df, z, region)
+
+    lon = df.iloc[repair_idx].longitude.to_numpy()
+    lat = df.iloc[repair_idx].latitude.to_numpy()
+    p = df.iloc[repair_idx].log_population.to_numpy()
+    y_bound = y_bound[np.where(z == 1)[0]]
+
+    c = c[idx]
+    c = c[np.where(z == 1)[0]]
+
+    # To adjust the size of the marker, I'm interested in the ratio of
+    # dollar / log person -- i.e., how efficient is this repair
+    # for the given location (smaller is better)
+    ratio = c / p
+
+    plt.rcParams.update({
+        'text.usetex': True,
+        'text.latex.preamble': r'\usepackage{amsfonts}'
+    })
+    budget_str = '{:,}'.format(key[1])
+
+    ax.plot(*coords, color='black')
+    ax.scatter(lon, lat, c=y_bound, s=ratio, alpha=0.5, cmap='winter')
+    ax.text(0.6, 0.9, s=rf'$\Gamma = {key[0]}$', size=20, transform=ax.transAxes)
+    ax.text(0.6, 0.85, s=rf'$B = \${budget_str}$', size=20, transform=ax.transAxes)
+    fig.colorbar(ScalarMappable(cmap='winter'), ax=ax).set_label(r'$\hat y_{i,l}$', fontsize=24)
+    return ax
+
+
 if __name__ == '__main__':
     xfile = '/home/zblanks/Documents/uva/fall21/bayesian-ml/project/data/X.csv'
     yfile = '/home/zblanks/Documents/uva/fall21/bayesian-ml/project/data/y.csv'
@@ -87,37 +139,62 @@ if __name__ == '__main__':
 
     # Compute the WAIC comparison between the empirical and uninformed models
     model_dict = compare_models(X, regions, y)
-    waic_df = az.compare(
-        {'uninformed': model_dict['uninformed'], 'empirical': model_dict['empirical']},
-        ic='waic', scale='deviance'
-    )
+    # waic_df = az.compare(
+    #     {'uninformed': model_dict['uninformed'], 'empirical': model_dict['empirical']},
+    #     ic='waic', scale='deviance'
+    # )
 
-    if not os.path.isdir('results'):
-        os.mkdir('results')
+    # if not os.path.isdir('results'):
+    #     os.mkdir('results')
     
-    waic_df.to_csv('results/waic.csv')
+    # waic_df.to_csv('results/waic.csv')
 
-    # Simple Forest plot comparing the learned interecept distribution for each
-    # region
-    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
-    az.plot_forest(model_dict['uninformed'], var_names=['α'], ax=axes[0])
-    az.plot_forest(model_dict['empirical'], var_names=['α'], ax=axes[1])
-    axes[0].set_ylabel('Uninformed Prior', fontsize=20)
-    axes[1].set_ylabel('Empirical Prior', fontsize=20)
-    plt.savefig('results/forestplot.pdf', dpi=300, bbox_inches='tight')
+    # # Simple Forest plot comparing the learned interecept distribution for each
+    # # region
+    # fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+    # az.plot_forest(model_dict['uninformed'], var_names=['α'], ax=axes[0])
+    # az.plot_forest(model_dict['empirical'], var_names=['α'], ax=axes[1])
+    # axes[0].set_ylabel('Uninformed Prior', fontsize=20)
+    # axes[1].set_ylabel('Empirical Prior', fontsize=20)
+    # plt.savefig('results/forestplot.pdf', dpi=300, bbox_inches='tight')
 
-    report = check_model_performance(X, regions, y, model_dict['uninformed'])
-    report_df = pd.DataFrame(report).transpose()
-    report_df.to_csv('results/classification_report.csv')
+    # report = check_model_performance(X, regions, y, model_dict['uninformed'])
+    # report_df = pd.DataFrame(report).transpose()
+    # report_df.to_csv('results/classification_report.csv')
 
     # Finally use the prediction bounds to solve the optimization problem
     # for various budgets and degrees of robustness
     if not os.path.exists('results/optimal_repairs.pkl'):
         z_star = fix_waterpoints(X, regions, model_dict['uninformed'], df,
-                                 [2, 3], [10000, 100000])
+                                 [2, 3], [1000, 10000])
         
         with open('results/optimal_repairs.pkl', 'wb') as file:
             dill.dump(z_star, file)
     else:
         with open('results/optimal_repairs.pkl', 'rb') as file:
             z_star = dill.load(file)
+
+    # Using the computed optimal repairs, let's see what the algorithm
+    # decided and then plot it on a map
+    shp_file = '/home/zblanks/Documents/uva/fall21/bayesian-ml/project/data/tza/tza.shp'
+    sf = shp.Reader(shp_file)
+    y_bound = create_prediction_interval(X, regions, model_dict['uninformed'])
+    des_coords = get_region_bounds(sf)
+    rng = np.random.default_rng(17)
+    c_bar = rng.uniform(100., 2000., size=X.shape[0])
+    keys = [(2, 10000), (3, 10000)]
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
+    for i in range(2):
+        plot_repairs(
+            df=df,
+            y_bound=y_bound,
+            z_star=z_star,
+            key=keys[i],
+            coords=des_coords,
+            c=c_bar,
+            fig=fig,
+            ax=axes[i]
+        )
+
+    plt.savefig('results/repairs.pdf', dpi=300, bbox_inches='tight')
